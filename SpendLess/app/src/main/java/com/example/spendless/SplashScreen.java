11a46b4ae5089ca1159bb1efb14bd29bc04b280e
Ansel Philip Gonsalves <anselgons22@gmail.com>
2020-03-09 10:05:52 +0530

    Initial Commit

---
+++ SpendLess/app/src/main/java/com/example/spendless/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.example.spendless;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
